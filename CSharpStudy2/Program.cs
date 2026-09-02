f0ca4c48472daaebf177be9d28904c32e914acc4
EZEN\EZENAC <ashleyriot000@gmail.com>
2026-09-02 15:32:12 +0900

    C# 프로그래밍 기초 1일차

---
+++ CSharpStudy2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpStudy2
+{
+    internal class Program
+    {
+        //진입점(Entry Point)
+        //프로그램은 Main함수에서 시작해서 Main함수로 끝난다.
+        static void Main(string[] args)
+        {
+            //Console.Write("당신의 이름은 무엇입니까? ");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("정말 당신의 이름이 " + name + "입니까?");
+
+            //먼저 이름을 물어봄.
+            //나이를 물어봄.
+            //국어 성적을 물어봄.
+            //수학 성적. 영어 성적을 물어봄.
+            //당신은 누구이며, 나이는 얼마고, 국어~, 수학~, 영어은 얼마입니다.
+        }
+    }
+}
