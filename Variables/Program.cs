554e4a69f9d33e1fec641180318760f51ba759fd
EZEN\EZENAC <ashleyriot000@gmail.com>
2026-09-02 17:58:51 +0900

    형변환, 오버플로우

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -39,6 +39,31 @@
             {
                 Console.WriteLine("아니요, 결과값이 1과 다릅니다.");
             }
+
+            //+,-,*,/,%
+
+            Console.Write("국어성적을 입력해주세요 : ");
+            //string koreanText = Console.ReadLine();
+            //int korean = int.Parse(koreanText);
+            int korean = int.Parse(Console.ReadLine());
+            Console.Write("영어성적을 입력해주세요 : ");
+            int english = int.Parse(Console.ReadLine());
+            Console.Write("수학성적을 입력해주세요 : ");
+            int math = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("국어 성적은 " + korean + "점이고,\n 영어 성적은 " + 
+                english + "점이고,\n 수학점수는 " + math + "점입니다." );
+            Console.WriteLine("전체 점수의 총합은 " + (korean + english + math));
+            Console.WriteLine("평균 점수는 " + (korean + english + math)/3);
+            Console.WriteLine($"평균 점수(실수)는 {(korean + english + math) / 3f:F2}");
+            Console.WriteLine($"평균 점수(실수)는 {1 / 3f:P2}");
+
+            //오버플로우 : 예상한 결과가 나오지 않아 원하는 동작을 하지 않을 가능성이 생김. 
+            long lValue = 1000000000000000;
+            int iValue = (int)lValue;
+
+            Console.WriteLine(lValue);
+            Console.WriteLine(iValue);
         }
     }
 }
